8ce3b41b334a95d92850b4a806597de375e62ad4
bruno falis <brunofalis0@gmail.com>
2018-10-16 17:06:45 -0300

    t

--- html/climinha/index.js
+++ html/climinha/index.js
@@ -12,11 +12,9 @@ $(function() {
             method: 'GET',
             success: function(response){
                 $.each(response, function(i, e){
-                    var div = $('<div style="border: 1px solid black;width:200px;height:100px">'
-                    +'<img src="http://openweathermap.org/img/w/10d.png" height="50" width="50">'
-                                      +'<p>'+e.tempMin+' Cº</p>'
-                                      +'<p>'+e.tempMax+' Cº</p>'
-                                      +'<p>'+e.data+'</p>'
+                    var div = $('<div style="border: 1px solid black;width:300px;height:100px">'
+                                      +'<img src="http://openweathermap.org/img/w/10d.png" height="50" width="50">'
+                                      +'<p>' + e.tempMin + ' Cº - ' + e.tempMax + ' Cº ' + e.data + '</p>'
                                +'<div>');
                     $('#container').append(div);
                 });
